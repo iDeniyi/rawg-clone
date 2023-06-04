589d41daf27d4a10b86e0306c333fb283659ab83
iDeniyi <inioluwa@ualberta.cai>
2023-06-04 09:47:07 -0600

    added search input componrnrt for searching for games

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,19 @@
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import React from "react";
+import { BsSearch } from "react-icons/bs";
+
+const SearchInput = () => {
+    return (
+        <InputGroup>
+            {/* <InputLeftElement children={<BsSearch />} /> */}
+            <Input
+                borderRadius={20}
+                placeholder="search games..."
+                padding="10px"
+                variant="filled"
+            />
+        </InputGroup>
+    );
+};
+
+export default SearchInput;
